42b4129b6cc90aa42a35740066a22b432c64b7ff
torerov <torerov39@gmail.com>
2016-01-24 18:32:28 +0000

    게시글 작성 기능 구현

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,15 @@ class HomeController < ApplicationController
   end
   
   def board
+       @post_list = Post.all.reverse
+  end
+  
+  def write_post
+   if request.method == "POST"
+      @post = Post.create(
+                          title: params[:title],
+                          content: params[:content])
+      redirect_to "/home/board"
+    end
   end
 end
