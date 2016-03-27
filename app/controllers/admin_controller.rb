7473011bad3a99a926ba7a6a594dd1077cf1fe35
choijaewoong <jwchoi0129@gmail.com>
2016-03-27 18:00:05 +0900

    admin 페이지 생성 및 기능 추가 * 유저 정보 수정 , 유저 삭제 기능 구현

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,24 @@
 class AdminController < ApplicationController
   def user_list
+    @user_list = User.all
+  end
+
+  # 유저 정보 수정
+  def user_modify
+    selected_user = User.find(params[:user_id])
+    selected_user.name = params[:user_name]
+    selected_user.major = params[:user_major]
+    selected_user.phone_number = params[:user_phonenumber]
+    selected_user.role = params[:user_role]
+    selected_user.introduction = params[:user_introduction]
+    selected_user.save
+    redirect_to :back
+  end
+
+  # 유저 삭제
+  def user_delete
+    selected_user = User.find(params[:user_id])
+    selected_user.destroy
+    redirect_to :back
   end
 end
