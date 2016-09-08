cac389ef5a7266364c17323f92e2b3172e957068
choijaewoong <jwchoi0129@gmail.com>
2016-09-09 00:58:10 +0900

    code refactoring 및 기능 추가 • board page 코드 수정 • 쓰기 권한 메시지 팝업 기능 구현

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -2,11 +2,15 @@ class BoardController < ApplicationController
   def list # 게시글 리스트
        @post_list = Post.all.reverse
   end
+  def check_write_authority
+    if !(user_signed_in? && current_user.role == "admin")
+      redirect_to :back, :flash => { :alert => "작성 권한이 없습니다." }
+    else
+      redirect_to "/board/write_post"
+    end
+  end
 
   def write_post # 게시글 작성
-    if !(user_signed_in? && current_user.role == "admin")
-      redirect_to '/'
-    end
     if request.method == "POST"
       @post = Post.create(
                           board_id: params[:id],
