2a61d5d4637e96e40f95fdc9fdb1ff392f8b8f53
choijaewoong <jwchoi0129@gmail.com>
2016-09-09 11:29:52 +0900

    code refactoring • guest page 코드 수정

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -4,13 +4,16 @@ class BoardController < ApplicationController
   end
   def check_write_authority
     if !(user_signed_in? && current_user.role == "admin")
-      redirect_to :back, :flash => { :alert => "작성 권한이 없습니다." }
+      redirect_to "/board/list", :flash => { :alert => "작성 권한이 없습니다." }
     else
       redirect_to "/board/write_post"
     end
   end
 
   def write_post # 게시글 작성
+    if !(user_signed_in? && current_user.role == "admin")
+      redirect_to "/board/list", :flash => { :alert => "작성 권한이 없습니다." }
+    end
     if request.method == "POST"
       @post = Post.create(
                           board_id: params[:id],
